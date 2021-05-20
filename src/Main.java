a12c7c1bd8ba04e8ff40c6075d2cdb40afaa84e2
David Subhan <subhan@SPSEJECNANET.CZ>
2021-05-20 11:17:50 +0200

    Hotový projekt

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+
+import java.io.*;
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		File file = new File("lm.txt");
+		FileInputStream fs = new FileInputStream(file);
+        InputStreamReader in = new InputStreamReader(fs);
+        BufferedReader br = new BufferedReader(in);
+          
+        String radek;
+          
+        int pocetSlov = 0;
+       
+        int pocetMezer = 0;
+          
+       
+        while((radek = br.readLine()) != null)
+        {
+        	
+        	String[] slova = radek.split("\\s+");
+        	pocetSlov += slova.length;
+        	
+        	for(int a=0; a<radek.length(); a++) {
+        		if(radek.charAt(a)==' ') {
+        			pocetMezer++;
+        		}
+        	}
+         
+        }
+        
+        System.out.println("Počet slov = " +pocetSlov);
+        System.out.println("Počet mezer = " +pocetMezer);
+          
+     
+       
+	}
+
+}
